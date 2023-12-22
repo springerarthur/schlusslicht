021bd6372a7bc747c705d67e7c23beb07689ac09
Arthur <springer.arthur@gmail.com>
2023-12-23 00:19:36 +0100

    delete old test file

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,18 +149,18 @@ export default function Home({ overview }: InferGetServerSidePropsType<typeof ge
           <div id="progress-container">
             <div className="progress">
               <div id="progress-bar-bike" className="progress-bar" role="progressbar" aria-valuenow={50} aria-valuemin={0} aria-valuemax={100} style={{ 'width': percentBike + '%' }}>
-                <div className="progress-text text1">{team1_bike.toFixed(1)}</div>
+                <div className="progress-text text1">{team1_bike.toFixed(3)}</div>
                 <div className="progress-text text2">🚴</div>
-                <div className="progress-text text3">{team2_bike.toFixed(1)}</div>
+                <div className="progress-text text3">{team2_bike.toFixed(3)}</div>
               </div>
             </div>
           </div>
           <div id="progress-container">
             <div className="progress">
               <div id="progress-bar-run" className="progress-bar" role="progressbar" aria-valuenow={50} aria-valuemin={0} aria-valuemax={100} style={{ 'width': percentRun + '%' }} >
-                <div className="progress-text text1">{team1_run.toFixed(2)}</div>
+                <div className="progress-text text1">{team1_run.toFixed(3)}</div>
                 <div className="progress-text text2">🏃</div>
-                <div className="progress-text text3">{team2_run.toFixed(2)}</div>
+                <div className="progress-text text3">{team2_run.toFixed(3)}</div>
               </div>
             </div>
           </div>

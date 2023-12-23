2ecb7952911ad725b7631fac33b6931d1d4f4146
Arthur <springer.arthur@gmail.com>
2023-12-23 22:06:19 +0100

    change cron job to test deployment error

--- pages/api/test.tsx
+++ pages/api/test.tsx
@@ -1,15 +1,19 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+// import type { NextApiRequest, NextApiResponse } from 'next';
  
-export default async function handler(
-  request: NextApiRequest,
-  response: NextApiResponse,
-) {
-  const result = await fetch(
-    'http://worldtimeapi.org/api/timezone/America/Chicago',
-  );
-  const data = await result.json();
+// export default async function handler(
+//   request: NextApiRequest,
+//   response: NextApiResponse,
+// ) {
+//   const result = await fetch(
+//     'http://worldtimeapi.org/api/timezone/America/Chicago',
+//   );
+//   const data = await result.json();
 
-  console.log(data);
+//   console.log(data);
  
-  return response.json({ datetime: data.datetime });
+//   return response.json({ datetime: data.datetime });
+// }
+export default function handler(req, res) {
+  console.log("Hello Cron.");
+  res.status(200).end('Hello Cron!');
 }

edf5b492de18b572db3e462262499d212fb7bf86
Arthur <springer.arthur@gmail.com>
2024-01-02 22:19:07 +0100

    Fix garmin sync issue

--- lib/GarminConnectSync.ts
+++ lib/GarminConnectSync.ts
@@ -1,81 +1,101 @@
 import { IActivity } from "garmin-connect/dist/garmin/types";
 import { GarminConnect } from "garmin-connect";
-import { SportTypeIds, getActivitiesForUserURL } from './GarminConstants';
-import { Users } from "../datastore/Users";
+import { SportTypeIds, getActivitiesForUserURL } from "./GarminConstants";
+import { Roland, Users } from "../datastore/Users";
 import ActivityService from "./ActivityService";
 import ConfigurationService from "./ConfigurationService";
 
 export default class GarminConnectSync {
+  readonly syncInterval: number = 15;
 
-    readonly syncInterval: number = 15;
+  private activityService = new ActivityService();
+  private configurationService = new ConfigurationService();
 
-    private activityService = new ActivityService();
-    private configurationService = new ConfigurationService();
+  public async updateRequired(): Promise<boolean> {
+    const lastUpdateTimeStamp =
+      await this.configurationService.getLastUpdateTimeStamp();
 
-    public async updateRequired(): Promise<boolean> {
-        const lastUpdateTimeStamp = await this.configurationService.getLastUpdateTimeStamp();
+    const now = new Date();
+    const differenceInMs = now.getTime() - lastUpdateTimeStamp.getTime();
 
-        const now = new Date();
-        const differenceInMs = now.getTime() - lastUpdateTimeStamp.getTime();
+    const differenceInMinutes = differenceInMs / (1000 * 60);
 
-        const differenceInMinutes = differenceInMs / (1000 * 60);
+    return differenceInMinutes > this.syncInterval;
+  }
 
-        return differenceInMinutes > this.syncInterval;
+  public async importDataFromGarminConnect(force: boolean = false) {
+    if (!force && process.env.DISABLE_AUTOUPDATE) {
+      return;
     }
 
-    public async importDataFromGarminConnect(force: boolean = false) {
-        if (!force && process.env.DISABLE_AUTOUPDATE) {
+    try {
+      if (force || (await this.updateRequired())) {
+        await this.importData();
+
+        await this.configurationService.upsertLastUpdateTimeStamp();
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  private async importData() {
+    const GCClient = new GarminConnect({
+      username: process.env.GARMIN_EMAIL + "",
+      password: process.env.GARMIN_PWD + "",
+    });
+    await GCClient.login();
+
+    const activitiesFromDb = await this.activityService.getActivities();
+
+    Users.forEach(async user => {
+        await this.addActivitiesIfNotExists(activitiesFromDb, GCClient, user.garminUserId);
+    });
+  }
+
+  private async addActivitiesIfNotExists(
+    activitiesFromDb: IActivity[],
+    gcClient: GarminConnect,
+    garminConnectUserId: string
+  ) {
+    try {
+      const activitiesFromGarmin = (
+        await gcClient.get<any>(getActivitiesForUserURL + garminConnectUserId, {
+          params: { start: 0, limit: 5 },
+        })
+      ).activityList as IActivity[];
+
+      await Promise.all(
+        activitiesFromGarmin.map(async (activityFromGarmin) => {
+          if (
+            activityFromGarmin.sportTypeId !== SportTypeIds.running &&
+            activityFromGarmin.sportTypeId !== SportTypeIds.bike &&
+            activityFromGarmin.sportTypeId !== SportTypeIds.swimming
+          ) {
             return;
-        }
+          }
 
-        try {
-            if (force || await this.updateRequired()) {
-                await this.importData();
+          if (
+            activitiesFromDb.some(
+              (activityFromDb) =>
+                activityFromDb.activityId == activityFromGarmin.activityId
+            )
+          ) {
+            return;
+          }
 
-                await this.configurationService.upsertLastUpdateTimeStamp();
-            }
-        } catch (e) {
-            console.error(e);
-        }
+          const garminStartDate = new Date(activityFromGarmin.startTimeLocal);
+          let now = new Date();
+          if (
+            garminStartDate.getFullYear() === now.getFullYear() &&
+            garminStartDate.getMonth() === now.getMonth()
+          ) {
+            await this.activityService.insertActivity(activityFromGarmin);
+          }
+        })
+      );
+    } catch (e) {
+      console.error(e);
     }
-
-    private async importData() {
-        const GCClient = new GarminConnect({
-            username: process.env.GARMIN_EMAIL + "",
-            password: process.env.GARMIN_PWD + ""
-        });
-        await GCClient.login();
-
-        const activitiesFromDb = await this.activityService.getActivities();
-
-        Users.forEach(async user => {
-            await this.addActivitiesIfNotExists(activitiesFromDb, GCClient, user.garminUserId);
-        });
-    }
-
-    private async addActivitiesIfNotExists(activitiesFromDb: IActivity[], gcClient: GarminConnect, garminConnectUserId: string) {
-        try {
-            const activitiesFromGarmin = (await gcClient.get<any>(getActivitiesForUserURL + garminConnectUserId, {
-                params: { start: 0, limit: 5 }
-            })).activityList as IActivity[];
-
-            for (const activityFromGarmin of activitiesFromGarmin) {
-                if (activityFromGarmin.sportTypeId !== SportTypeIds.running && activityFromGarmin.sportTypeId !== SportTypeIds.bike && activityFromGarmin.sportTypeId !== SportTypeIds.swimming) {
-                    continue;
-                }
-
-                if (activitiesFromDb.some(activityFromDb => activityFromDb.activityId == activityFromGarmin.activityId)) {
-                    continue;
-                }
-
-                const garminStartDate = new Date(activityFromGarmin.startTimeLocal);
-                let now = new Date();
-                if (garminStartDate.getFullYear() === now.getFullYear() && garminStartDate.getMonth() === now.getMonth()) {
-                    await this.activityService.insertActivity(activityFromGarmin);
-                }
-            }
-        } catch (e) {
-            console.error(e);
-        }
-    }
+  }
 }

5e8d89876371b4fcf11a457a70a60b74c1025bae
Arthur <springer.arthur@gmail.com>
2024-01-11 23:56:07 +0100

    bug: fix images for profile page

--- src/datastore/Users.ts
+++ src/datastore/Users.ts
@@ -3,32 +3,32 @@ import { User } from "../lib/User";
 export const Arthur: User = {
     displayName: "Arthur",
     garminUserId: "Springer.arthur",
-    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/1bde3ed0-0e37-4b70-84c8-f30ab03fd87e-36151194.png"
+    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/39e4e389-7cd9-4489-818f-700c119a95f6-36151194.png"
 };
 export const Daniel: User = {
     displayName: "Daniel",
     garminUserId: "6d29db1b-2cc2-4d32-b63c-479a1fa38471",
-    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/cbde8656-f4ff-4e8a-a5bb-f3ca90258430-prth.png"
+    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/cbde8656-f4ff-4e8a-a5bb-f3ca90258430-prof.png"
 }
 export const Roland: User = {
     displayName: "Roland",
     garminUserId: "97e62216-5e5d-42b2-a7d1-f9452a5363b4",
-    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/7a2aae75-5515-4a29-9d83-c7c0d169dc7b-94018658.png"
+    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/405a6a2b-4adc-4ccf-bde2-680924fd65af-94018658.png"
 };
 export const Waldi: User = {
     displayName: "Bob",
     garminUserId: "f8d6b455-0aec-4c46-b3e1-5621cac1719f",
-    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/7912da40-1d7e-43de-83fb-84f74473998d-89799785.png"
+    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/89c4576d-57f2-472e-962f-b023cfca64e3-89799785.png"
 };
 export const AlexH: User = {
     displayName: "Alex H.",
     garminUserId: "1690439f-a46f-4079-9709-33ffa337c80c",
-    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/05d44d12-621e-48eb-a7ab-9e64717ee534-prth.png"
+    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/05d44d12-621e-48eb-a7ab-9e64717ee534-prof.png"
 };
 export const AlexS: User = {
     displayName: "Alex S.",
     garminUserId: "skipper2193",
-    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/e87ca23b-2911-4334-882b-af3cbe8a24b5-17108593.png"
+    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/13f052f0-3359-4a4b-ac2d-1dba9fcf02ac-17108593.png"
 };
 export const Jan: User = {
     displayName: "Jan",
@@ -38,7 +38,7 @@ export const Jan: User = {
 export const Thomas: User = {
     displayName: "Thomas",
     garminUserId: "b936a5d7-ed2f-4ca6-a535-25e6d25707bc",
-    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/5a2db0c7-a725-4fa5-a281-2c47637aa443-72292761.png"
+    profileImg: "https://s3.amazonaws.com/garmin-connect-prod/profile_images/16415f83-a6b9-4f4f-9831-807efce8f5f5-72292761.png"
 };
 
 export const Users: User[] = [

2e513717dda17aadf370d45e44de5b615ca00a40
Arthur <springer.arthur@gmail.com>
2024-02-27 08:54:29 +0100

    add logging

--- src/utilities/serviceWorker.ts
+++ src/utilities/serviceWorker.ts
@@ -1,17 +1,24 @@
+import logToServer from "./logger";
+
 export async function registerServieWorker() {
+  logToServer("registerServieWorker");
   if (!("serviceWorker" in navigator)) {
+    logToServer("Service workers are not supported by this browser");
     throw Error("Service workers are not supported by this browser");
   }
+
+  logToServer("registerServieWorker success");
 
   await navigator.serviceWorker.register("serviceWorker.js");
 }
 
 export async function getReadyServiceWorker() {
-  console.log("getReadyServiceWorker");
+  logToServer("getReadyServiceWorker");
   if (!("serviceWorker" in navigator)) {
-    console.log("getReadyServiceWorker error: !serviceWorker in navigator");
+    logToServer("Service workers are not supported by this browser");
     throw Error("Service workers are not supported by this browser");
   }
 
+  logToServer("getReadyServiceWorker success");
   return navigator.serviceWorker.ready;
 }

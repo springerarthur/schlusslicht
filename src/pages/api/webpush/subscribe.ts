48f3b735856f9fdaf765bc27498c7290b7681689
Arthur <springer.arthur@gmail.com>
2024-02-27 08:24:06 +0100

    add logs for subscribe.ts

--- src/pages/api/webpush/subscribe.ts
+++ src/pages/api/webpush/subscribe.ts
@@ -5,22 +5,26 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  console.log("subscribe start");
   if (request.method !== "POST") {
     response.status(405).json({});
     return;
   }
 
+  console.log("subscribe POST");
   try {
     const subscription: PushSubscription | undefined = await JSON.parse(request.body);
     if(subscription === undefined) {
       response.status(400).json({});
       return;
     }
+    console.log("subscription: " + JSON.stringify(subscription));
 
     const webPushSubscriptionService = new WebPushSubscriptionService();
     webPushSubscriptionService.saveSubscription(
       subscription
     );
+    console.log("subscription saved");
 
     response.status(200).json({});
   } catch (error) {
